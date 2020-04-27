4def19dfb7f3a0ffcf074519e6a128e1f268f9d7
xahram <ssasuke1163@gmail.com>
2020-04-27 22:41:57 +0500

    Implementing Chat

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const io = require('socket.io')
+
 require('../db/mongoose')
 
 const path = require('path')
@@ -18,11 +20,17 @@ app.use(postsRouter)
 
 
 
+const server = require('http').Server(app)
+const socket = io(server)
 
+socket.on('connection',(socket)=>{
+    console.log("Socket ", socket)
+})
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../front/build/index.html'));
 });
 
-app.listen(port, () => { console.log(`Listening on port ${port}`)
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 })
